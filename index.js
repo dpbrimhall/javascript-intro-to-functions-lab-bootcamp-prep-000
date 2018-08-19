91869d375b00544d278d4eb9fd97ba62d5267827
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-19 20:18:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+logShout('hello')
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+logWhisper('HELLO')
+
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello'
+  if (lowercase.toLowerCase() === lowercase) {
+    return "I can't hear you!" 
+    
+  }
+}
+ 
+ 
+sayHiToGrandma('hello')
+sayHiToGrandma('HELLO')
+
+ 
